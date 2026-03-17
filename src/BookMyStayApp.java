00065b5807e62f80d47ab39a5343bbf2e004486b
AkhilKotlur <akhilbummy2006ak@gmail.com>
2026-03-17 15:19:13 +0530

    UC1: Add welcome message module

---
+++ src/BookMyStayApp.java
@@ -0,0 +1,11 @@
+public class BookMyStayApp{
+
+    /**
+     * Application entry point.
+     * * This method is the first method executed
+     */
+    public static void main(String[] args) {
+        // Displaying the welcome message
+        System.out.println("Welcome to the Hotel Booking Management System");
+        System.out.println("System initialized successfully.");
+    }}
